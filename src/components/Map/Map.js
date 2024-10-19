8b43ec5a8aaa6c0a3b283e77324db47ec9cfb716
extinctmule <extinctmule@gmail.com>
2024-10-19 19:04:33 +0900

    update: 위치 정보 api연결

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,5 +1,6 @@
 /* global kakao */
 import React, { useEffect, useRef } from 'react';
+import { getUserLocation } from '../services/locationService';
 
 const KAKAO_MAP_KEY = process.env.REACT_APP_KAKAO_MAP_KEY;
 
@@ -20,15 +21,25 @@ const loadKakaoMapScript = () => {
 
 const Map = () => {
   const mapRef = useRef(null);
+  const [userLocation, setUserLocation] = useState({ latitude: 37.400113, longitude: 127.106766 });
+
+  useEffect(() => {
+    getUserLocation()
+      .then((location) => {
+        setUserLocation(location);
+      })
+      .catch((error) => {
+        console.error('위치 정보를 가져오는 중 오류 발생', error);
+      });
+  }, []);
 
   useEffect(() => {
     loadKakaoMapScript()
       .then(() => {
         kakao.maps.load(() => {
-          //const container = document.getElementById("map");
           const container = mapRef.current;
           const options = {
-            center: new kakao.maps.LatLng(37.400113, 127.106766),
+            center: new kakao.maps.LatLng(userLocation.latitude, userLocation.longitude),
             level: 3, // 디폴트3, 1~4 조정가능
           };
           const map = new kakao.maps.Map(container, options);
@@ -37,7 +48,7 @@ const Map = () => {
       .catch((error) => {
         console.error(error);
       });
-  }, []);
+  }, [userLocation]);
 
   return <div ref={mapRef} id="map" style={{ width: '100vw', height: '92vh' }}></div>;
 };

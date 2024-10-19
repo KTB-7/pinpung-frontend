a44089b8458275d092394ba54244d709a53196db
extinctmule <extinctmule@gmail.com>
2024-10-19 19:05:03 +0900

    update: 디버깅용 console.log 추가

--- src/services/locationService.js
+++ src/services/locationService.js
@@ -1,15 +1,19 @@
 export const getUserLocation = () => {
   return new Promise((resolve, reject) => {
     if (!navigator.geolocation) {
-      reject(new Error('Geolocation 미제공'));
+      reject(new Error('Geolocation 미제공 브라우저'));
     }
 
     navigator.geolocation.getCurrentPosition(
       (postion) => {
         const { latitude, longitude } = postion.coords;
+        console.log(`위도: ${latitude}, 경도: ${longitude}`);
         resolve({ latitude, longitude });
       },
-      (error) => reject(error),
+      (error) => {
+        reject(error);
+        console.log(`위치 정보 요청 실패: ${error.message}`);
+      },
     );
   });
 };

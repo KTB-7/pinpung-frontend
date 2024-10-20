765a0735c0e1be961e4a3c63239c6d48dc1bb949
extinctmule <extinctmule@gmail.com>
2024-10-20 21:16:08 +0900

    chore: 주석 및 디버깅 편의 추가

--- src/services/kakaoMapService.js
+++ src/services/kakaoMapService.js
@@ -1,4 +1,6 @@
-/*global kakao*/
+/* 특정 좌표를 기준으로 '카페' 카테고리로 사용자 주변 카페를 검색함 */
+
+/* global kakao */
 export const fetchCafes = (center) => {
   return new Promise((resolve, reject) => {
     const ps = new kakao.maps.services.Places();
@@ -8,7 +10,7 @@ export const fetchCafes = (center) => {
         if (status === kakao.maps.services.Status.OK) {
           resolve(data);
         } else {
-          reject('카페 데이터를 가져오는 데 실패했습니다.');
+          reject('사용자 주변 카페 데이터를 가져오는 데 실패했습니다.');
         }
       },
       { location: center },

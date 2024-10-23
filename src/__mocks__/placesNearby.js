03af5883b1f8bd0b16ed51f92ef76c2f87c0b8c0
extinctmule <extinctmule@gmail.com>
2024-10-23 18:55:49 +0900

    add: places/nearby 목업데이터 추가

---
+++ src/__mocks__/placesNearby.js
@@ -0,0 +1,19 @@
+export const placesNearByDataSet = {
+  results: [
+    {
+      placeId: 1,
+      imageUrl:
+        'https://images.unsplash.com/photo-1509042239860-f550ce710b93?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      placeId: 2,
+      imageUrl:
+        'https://plus.unsplash.com/premium_photo-1673545518947-ddf3240090b1?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      placeId: 3,
+      imageUrl:
+        'https://images.unsplash.com/photo-1461010083959-8a5727311252?q=80&w=3224&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+  ],
+};

e2b67f9591c2e1a657fcb797d5960c750f995bb8
extinctmule <extinctmule@gmail.com>
2024-10-23 18:56:14 +0900

    add: places/placeId 목업데이터 추가

---
+++ src/__mocks__/placesPlaceId.js
@@ -0,0 +1,46 @@
+export const placesPlaceId = {
+  results: [
+    {
+      placesId: 1,
+      placeName: '핑펑카페',
+      tags: ['조용함', '카공'],
+      reviews: [
+        {
+          reviewId: 101,
+          userId: 1,
+          text: '리뷰텍스트임',
+          uploadedTime: '2024-10-07',
+        },
+      ],
+      pung: {
+        pungId: 201,
+        userId: 1,
+        placeId: 1,
+        imageUrl: '이미지 URL',
+        text: '대표 펑 텍스트임',
+        uploadedTime: '2024-10-07',
+      },
+    },
+    {
+      placesId: 2,
+      placeName: '펑 없는 카페',
+      tags: ['조용함', '카공'],
+      reviews: [
+        {
+          reviewId: 102,
+          userId: 2,
+          text: '리뷰텍스트임',
+          uploadedTime: '2024-10-08',
+        },
+      ],
+      pung: {
+        pungId: 202,
+        userId: 2,
+        placeId: 2,
+        imageUrl: '이미지 URL',
+        text: '대표 펑 텍스트임',
+        uploadedTime: '2024-10-08',
+      },
+    },
+  ],
+};

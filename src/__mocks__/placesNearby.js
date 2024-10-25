44a41d2249aa0e1ff9503621f2a42011de164a5e
extinctmule <extinctmule@gmail.com>
2024-10-25 18:51:56 +0900

    fix: placeID string으로 변경

--- src/__mocks__/placesNearby.js
+++ src/__mocks__/placesNearby.js
@@ -1,19 +1,37 @@
 export const placesNearByDataSet = {
   results: [
     {
-      placeId: 17127061, // 스타버그 판교H스퀘어점
+      placeId: '17127061', // 스타벅스 판교H스퀘어점
       imageUrl:
         'https://images.unsplash.com/photo-1509042239860-f550ce710b93?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      x: '127.10894179597794',
+      y: '37.401249861157744',
     },
     {
-      placeId: 1702979109, // 아이스걸크림보이 판교점
+      placeId: '1702979109', // 아이스걸크림보이 판교점
       imageUrl:
         'https://plus.unsplash.com/premium_photo-1673545518947-ddf3240090b1?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      x: '127.1072085990955',
+      y: '37.40077120123289',
     },
     {
-      placeId: 21339034, // 투썸플레이스 판교테크노밸리점
+      placeId: '21339034', // 투썸플레이스 판교테크노밸리점
       imageUrl:
         'https://images.unsplash.com/photo-1461010083959-8a5727311252?q=80&w=3224&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      x: '127.10851032513395',
+      y: '37.402008016343174',
+    },
+    {
+      placeId: '19660680', //공차
+      imageUrl: null,
+      x: '127.10682616301104',
+      y: '37.40187079272404',
+    },
+    {
+      placeId: '26805871', // 그라비티
+      imageUrl: null,
+      x: '127.10742068955716',
+      y: '37.40218741338368',
     },
   ],
 };

935566e9318825225beeb6f87040d9c9ba19d8fe
extinctmule <extinctmule@gmail.com>
2024-10-25 18:54:07 +0900

    fix: 목업데이터에 맞게 코드 수정

--- src/components/Markers/CafeMarker.js
+++ src/components/Markers/CafeMarker.js
@@ -1,36 +1,48 @@
 /* 카페 목록을 바탕으로 맵에 마커를 생성하고, 클릭 시 정보 창 표시 */
 
 /* global kakao */
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { fetchCafesByIds } from '../../services/placesService';
 
 const DEFAULT_MARKER_IMAGE =
   'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
 
 const CafeMarker = ({ cafes, map }) => {
   const markers = useRef({});
+  const [cafeData, setCafeData] = useState([]);
+
+  // 목업 API 호출하여 데이터 가져오기
+  useEffect(() => {
+    if (!map || cafes.length === 0) return;
+
+    fetchCafesByIds(cafes).then((data) => setCafeData(data));
+  }, [cafes, map]);
 
   useEffect(() => {
-    if (!map || !cafes.length === 0) return;
+    console.log('카페데이터:', cafeData);
+    if (!map || !cafeData.length === 0) return;
 
     const currentMarkers = markers.current;
+
     // 새롭게 가져온 카페 목록으로 마커 업데이트
-    cafes.forEach((cafe) => {
-      if (!currentMarkers[cafe.id]) {
-        // 중복 마커 방지
-        const imageUrl = cafe.image || DEFAULT_MARKER_IMAGE;
+    cafeData.forEach((place) => {
+      // 중복 마커 방지하기
+      if (!currentMarkers[place.placeId]) {
+        console.log(place);
+        const imageUrl = place.imageUrl || DEFAULT_MARKER_IMAGE;
         const imageSize = new kakao.maps.Size(24, 35);
         const markerImage = new kakao.maps.MarkerImage(imageUrl, imageSize);
 
         // 마커 생성
         const marker = new kakao.maps.Marker({
           map: map,
-          position: new kakao.maps.LatLng(cafe.y, cafe.x),
+          position: new kakao.maps.LatLng(place.y, place.x),
           image: markerImage,
         });
 
         // 정보 창 생성
         const infowindow = new kakao.maps.InfoWindow({
-          content: `<div>${cafe.place_name}</div>`,
+          content: `<div>${place.place_name}</div>`,
         });
 
         // const infomodal
@@ -41,7 +53,7 @@ const CafeMarker = ({ cafes, map }) => {
         });
 
         // 마커를 객체에 저장 (중복 방지)
-        currentMarkers[cafe.id] = marker;
+        currentMarkers[place.placeId] = marker;
       }
     });
 

56a4ea612b847793c0d85212d3fc7f0a92163b42
extinctmule <extinctmule@gmail.com>
2024-10-29 23:48:35 +0900

    add: 목업데이터 형식 변경

--- src/__mocks__/placesPlaceId.js
+++ src/__mocks__/placesPlaceId.js
@@ -1,45 +1,28 @@
 export const placesPlaceIdDataSet = {
   results: [
     {
-      placesId: 1,
-      placeName: '핑펑카페',
-      tags: ['조용함', '카공'],
-      reviews: [
-        {
-          reviewId: 101,
-          userId: 1,
-          text: '리뷰텍스트임',
-          uploadedTime: '2024-10-07',
-        },
-      ],
-      pung: {
-        pungId: 201,
-        userId: 1,
-        placeId: 1,
-        imageUrl: '이미지 URL',
-        text: '대표 펑 텍스트임',
-        uploadedTime: '2024-10-07',
+      placeId: 17127061,
+      address: '경기도 성남시 분당구 판교역로 231, 에이치스퀘어 (삼평동)',
+      tags: ['조용함', '매장이 넓음', '친절함'],
+      reviews: {
+        count: 1,
+        reviews: [
+          {
+            reviewId: 1,
+            userId: 1,
+            placeId: 17127061,
+            text: '조용하고 친절합니다',
+            createdAt: '2024-10-14T10:18:37',
+            updatedAt: '2024-10-20T15:51:27',
+          },
+        ],
       },
-    },
-    {
-      placesId: 2,
-      placeName: '펑 없는 카페',
-      tags: ['조용함', '카공'],
-      reviews: [
-        {
-          reviewId: 102,
-          userId: 2,
-          text: '리뷰텍스트임',
-          uploadedTime: '2024-10-08',
-        },
-      ],
-      pung: {
-        pungId: 202,
-        userId: 2,
-        placeId: 2,
-        imageUrl: '이미지 URL',
-        text: '대표 펑 텍스트임',
-        uploadedTime: '2024-10-08',
+      representativePung: {
+        pungId: 1,
+        userId: 1,
+        placeId: 17127061,
+        imageWithText:
+          'https://images.unsplash.com/photo-1509042239860-f550ce710b93?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       },
     },
   ],

181da5afed94ab96b37f58aa15dc6358676349f6
extinctmule <extinctmule@gmail.com>
2024-11-01 04:57:19 +0900

    add: msw관련 설정 미리 추가

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+// if (process.env.NODE_ENV === 'development') {
+//   const { worker } = require('./mocks/browser');
+//   worker.start();
+// }
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />

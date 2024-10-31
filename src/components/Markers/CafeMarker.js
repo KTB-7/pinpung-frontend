f22bb8ae2a71b266678a39947c23a5190070c2f2
extinctmule <extinctmule@gmail.com>
2024-11-01 01:10:40 +0900

    fix: 실제 api로 변경

--- src/components/Markers/CafeMarker.js
+++ src/components/Markers/CafeMarker.js
@@ -1,37 +1,32 @@
 /* 카페 목록을 바탕으로 맵에 마커를 생성하고, 클릭 시 정보 창 표시 */
 
 /* global kakao */
-import { useEffect, useRef, useState } from 'react';
-import { fetchCafesByIds } from '../../services/placesService';
+import { useEffect, useRef } from 'react';
 
 const DEFAULT_MARKER_IMAGE =
   'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
 
 const CafeMarker = ({ cafes, map, onMarkerClick }) => {
   const markers = useRef({});
-  const [cafeData, setCafeData] = useState([]);
 
-  // 목업 API 호출하여 데이터 가져오기
+  // 데이터 가져오기
   useEffect(() => {
     if (!map || cafes.length === 0) return;
 
-    fetchCafesByIds(cafes).then((data) => setCafeData(data));
-  }, [cafes, map]);
-
-  useEffect(() => {
-    //console.log('카페데이터:', cafeData);
-    if (!map || !cafeData.length === 0) return;
-
     const currentMarkers = markers.current;
 
-    // 새롭게 가져온 카페 목록으로 마커 업데이트
     cafeData.forEach((place) => {
       // 중복 마커 방지하기
       if (!currentMarkers[place.placeId]) {
         //console.log('place:', place);
-        const imageUrl = place.imageUrl || DEFAULT_MARKER_IMAGE;
-        const imageSize = new kakao.maps.Size(25, 40);
-        const markerImage = new kakao.maps.MarkerImage(imageUrl, imageSize);
+        const imageUrl = (() => {
+            if (place.hasPung) {
+                return place.imageWithText;
+            } else {
+                return DEFAULT_MARKER_IMAGE;
+            }
+        })();
+        const markerImage = new kakao.maps.MarkerImage(imageUrl, new kakao.maps.Size(25, 40));
 
         // 마커 생성
         const marker = new kakao.maps.Marker({
@@ -47,7 +42,7 @@ const CafeMarker = ({ cafes, map, onMarkerClick }) => {
 
         // 마커 클릭 이벤트 등록
         kakao.maps.event.addListener(marker, 'click', () => {
-          onMarkerClick(place); // 클릭 시 선택된 카페 전달
+          onMarkerClick(place.placeId); // 클릭 시 선택된 카페 전달
         });
 
         // 마커를 객체에 저장 (중복 방지)
@@ -60,7 +55,7 @@ const CafeMarker = ({ cafes, map, onMarkerClick }) => {
       Object.values(currentMarkers).forEach((marker) => marker.setMap(null));
       markers.current = {}; // 메모리 초기화
     };
-  }, [cafes, map, cafeData, onMarkerClick]);
+  }, [cafes, map, onMarkerClick]);
 
   return null; // UI 요소 없으므로 렌더링할 내용 없음
 };

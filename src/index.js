2125ae7b7655f9b621fe0cbb885617e13037d15e
extinctmule <extinctmule@gmail.com>
2024-11-04 01:55:25 +0900

    feat: 브라우저 라우터 설정을 App.js대신 index.js에서 하도록 변경

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -14,7 +15,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
 

64fe06dc3f0fddefa5de8c730e6a82efe56a9788
extinctmule <extinctmule@gmail.com>
2024-11-04 13:19:32 +0900

    fix: map 메서드를 반환값 없는 forEach 메서드로 변경

--- src/service-worker.js
+++ src/service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) =>
       Promise.all(
-        cacheNames.map((cacheName) => {
+        cacheNames.forEach((cacheName) => {
           if (!cacheWhitelist.includes(cacheName)) {
             return caches.delete(cacheName);
           }

a444d6c6de9c24a7876db706a509ab1ecb248ae3
extinctmule <extinctmule@gmail.com>
2024-11-05 19:54:54 +0900

    fix: url 라우팅 로직 일단 수정

--- src/App.js
+++ src/App.js
@@ -22,13 +22,10 @@ function App() {
   // }, [isBottomSheetOpen, selectedPlaceId, location.pathname, navigate]);
 
   useEffect(() => {
+    // 로직이 괴상하고 뭔가 억지스럽다고 생각함.. 수정 필요
     if (isBottomSheetOpen && selectedPlaceId) {
-      // BottomSheet가 열리면 해당 장소의 상세 페이지로 이동
-      if (!location.pathname.includes('upload-pung')) {
-        navigate(`/places/${selectedPlaceId}`);
-      }
+      navigate(`/places/${selectedPlaceId}`);
     } else if (!isBottomSheetOpen && location.pathname === `/places/${selectedPlaceId}`) {
-      // BottomSheet가 닫히고 특정 장소 페이지에 있을 경우 홈으로 리디렉션
       navigate('/');
     }
   }, [isBottomSheetOpen, selectedPlaceId, location.pathname, navigate]);

e316054e0307811683166b9cc1d7746c4fc97136
extinctmule <extinctmule@gmail.com>
2024-11-06 13:43:36 +0900

    refactor: 목업데이터 api별로 분리

---
+++ src/mocks/data/placesData.js
@@ -0,0 +1,14 @@
+export const placesData = {
+  placeName: `Mock Cafe`,
+  address: '서울시 강남구 가짜 주소',
+  tags: ['커피', '디저트', '편안함'],
+  representativePung: {
+    imageWithText: 'https://via.placeholder.com/300',
+  },
+  reviews: {
+    reviews: [
+      { reviewId: 1, text: '멋진 카페에요!', createdAt: '2023-10-31' },
+      { reviewId: 2, text: '커피 맛있고 분위기 좋음!', createdAt: '2023-11-01' },
+    ],
+  },
+};

381c4d3955f4cacdbdd7c254916cf63cf387ba53
extinctmule <extinctmule@gmail.com>
2024-11-06 17:29:05 +0900

    add: 테스트용 bearer 임시 추가

--- src/api/placesApi.js
+++ src/api/placesApi.js
@@ -10,13 +10,20 @@ export const fetchNearbyCafes = async (longitude, latitude, radius) => {
       y: latitude,
       radius,
     },
+    headers: {
+      Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+    },
   });
 
   return response.data;
 };
 
 export const fetchCafeDetails = async (placeId) => {
-  const response = await axios.get(`${API_URL}/api/places/${placeId}`);
+  const response = await axios.get(`${API_URL}/api/places/${placeId}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+    },
+  });
 
   return response.data;
 };

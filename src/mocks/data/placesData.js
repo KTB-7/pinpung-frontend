a8a104cd1ecbd87b017df403b46bda83992f0961
extinctmule <extinctmule@gmail.com>
2024-11-06 14:26:01 +0900

    chore: 목업데이터 파라미터 값 누락된 것 추가

--- src/mocks/data/placesData.js
+++ src/mocks/data/placesData.js
@@ -1,14 +1,15 @@
 export const placesData = {
-  placeName: `Mock Cafe`,
-  address: '서울시 강남구 가짜 주소',
-  tags: ['커피', '디저트', '편안함'],
-  representativePung: {
-    imageWithText: 'https://via.placeholder.com/300',
-  },
+  placeId: 22,
+  placeName: '평화다방 강남역점',
+  address: '서울 강남구 강남대로94길 10',
+  tags: ['커피', '디저트', '친절함'],
   reviews: {
     reviews: [
       { reviewId: 1, text: '멋진 카페에요!', createdAt: '2023-10-31' },
       { reviewId: 2, text: '커피 맛있고 분위기 좋음!', createdAt: '2023-11-01' },
     ],
+  },
+  representativePung: {
+    imageWithText: 'https://via.placeholder.com/300',
   },
 };

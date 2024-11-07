eccebed458dd06ffb5d850d58d370ad47917d989
extinctmule <extinctmule@gmail.com>
2024-11-07 15:40:09 +0900

    add: pungApi 에러 처리 추가

--- src/api/pungApi.js
+++ src/api/pungApi.js
@@ -3,9 +3,14 @@ import axios from 'axios';
 const API_URL = process.env.REACT_APP_API_URL;
 
 export const fetchPungs = async (placeId, page) => {
-  const response = await axios.get(`${API_URL}/api/pungs/${placeId}`, page);
+  try {
+    const response = await axios.get(`${API_URL}/api/pungs/${placeId}`, page);
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    console.error('펑 가져오기 실패:', error);
+    throw new Error('펑을 가져오는 데 실패했습니다.');
+  }
 };
 
 export const addPung = async (userId, placeId, imageWithText, pureImage, text) => {
@@ -17,11 +22,16 @@ export const addPung = async (userId, placeId, imageWithText, pureImage, text) =
   data.append('pureImage', pureImage); // 순수 이미지 파일 객체
   data.append('text', text);
 
-  const response = await axios.post(`${API_URL}/api/pungs/upload`, data, {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-  });
+  try {
+    const response = await axios.post(`${API_URL}/api/pungs/upload`, data, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
 
-  return response.status;
+    return response.data;
+  } catch (error) {
+    console.error('펑 업로드하기 실패:', error);
+    throw new Error('펑 업로드에 실패했습니다.');
+  }
 };

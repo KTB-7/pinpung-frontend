61ea3a307143327b08661975b6f967a092ca2958
extinctmule <extinctmule@gmail.com>
2024-11-07 17:30:39 +0900

    add: axiosInstanace 생성하고 withCredentials 추가

---
+++ src/api/axiosInstance.js
@@ -0,0 +1,10 @@
+/* axios instance 생성 파일 */
+
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  withCredentials: true,
+});
+
+export default instance;

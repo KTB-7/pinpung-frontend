16800349a7fa5190146a4203f4f17013ca28e0b3
extinctmule <extinctmule@gmail.com>
2024-11-07 15:40:01 +0900

    add: placesApi 에러 처리 추가

--- src/api/placesApi.js
+++ src/api/placesApi.js
@@ -2,23 +2,30 @@ import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-//
+// 사용자 주변 카페들 불러오기
 export const fetchNearbyCafes = async (longitude, latitude, radius) => {
-  //console.log('fetchNearbyCafes params:', longitude, latitude, radius);
-  const response = await axios.get(`${API_URL}/api/places/nearby`, {
-    params: {
-      x: longitude,
-      y: latitude,
-      radius,
-    },
-  });
-
-  return response.data;
+  try {
+    const response = await axios.get(`${API_URL}/api/places/nearby`, {
+      params: {
+        x: longitude,
+        y: latitude,
+        radius,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('주변 카페 정보 가져오기 실패', error);
+    throw new Error('주변 카페 정보를 불러올 수 없습니다.');
+  }
 };
-
-// 마커누르면 나오는 카페세부정보
+// 카페 세부정보 불러오기
 export const fetchCafeDetails = async (placeId) => {
-  const response = await axios.get(`${API_URL}/api/places/${placeId}`);
+  try {
+    const response = await axios.get(`${API_URL}/api/places/${placeId}`);
 
-  return response.data;
+    return response.data;
+  } catch (error) {
+    console.error('카페 상세 정보 불러오기 실패:', error);
+    throw new Error('카페 상세 정보를 불러올 수 없습니다.');
+  }
 };

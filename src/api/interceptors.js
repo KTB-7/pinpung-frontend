d2171f5907a86f43103be6fdb7713cb77818dfb8
extinctmule <extinctmule@gmail.com>
2024-11-08 21:39:55 +0900

    fix: no-cache 헤더 추가

--- src/api/interceptors.js
+++ src/api/interceptors.js
@@ -6,6 +6,14 @@ export const setupRequestInterceptor = () => {
   instance.interceptors.request.use(
     (config) => {
       const token = localStorage.getItem('accessToken');
+
+      config.headers['Cache-Control'] = 'no-cache';
+      config.headers['Pragma'] = 'no-cache';
+      config.headers['Expires'] = '0';
+
+      // 추가 보안 헤더
+      //config.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains; preload';
+
       if (token) {
         config.headers.Authorization = `Bearer ${token}`;
       }

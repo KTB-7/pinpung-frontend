f837027672752d09049f82ab10effe1d0f302541
extinctmule <extinctmule@gmail.com>
2024-11-08 21:36:36 +0900

    fix: Navbar 컴포넌트 App에서 렌더링하게 변경

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Map from '../components/Map/Map';
-import Navbar from '../components/NavBar';
 
 const Home = () => {
   return (
     <>
       <Map />
-      <Navbar />
     </>
   );
 };

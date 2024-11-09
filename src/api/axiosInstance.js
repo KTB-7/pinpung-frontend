ee4dd411040be5034d4ee20a239d71a2c5dc154f
Yoojung Choi <101849662+eqvyoo@users.noreply.github.com>
2024-11-09 16:30:23 +0900

    chore : changed timeout to 60s in axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   withCredentials: false,
-  timeout: 10000,
+  timeout: 60000,
 });
 
 export default instance;

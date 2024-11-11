28a2644e4095e470d393bd6c72a1d1b67ae0150d
extinctmule <extinctmule@gmail.com>
2024-11-11 14:14:37 +0900

    add: Map 컴포넌트 렌더링 위치 변경

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 import './styles/responsive.css';
 import useStore from './store';
 import { useEffect } from 'react';
-import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import { Routes, Route, useNavigate, useLocation, useParams } from 'react-router-dom';
 import Home from './pages/Home';
+import Map from './components/Map/Map';
 import Navbar from './components/Navbar';
 import BottomSheet from './components/BottomSheet';
 import UploadPung from './pages/UploadPung';
@@ -17,21 +18,22 @@ import UploadPung from './pages/UploadPung';
 
 function App() {
   // const { isBottomSheetOpen = false, selectedPlaceId = null, closeBottomSheet = true} = useStore();
-  const { isBottomSheetOpen, selectedPlaceId, closeBottomSheet } = useStore();
+  const { isBottomSheetOpen, selectedPlaceId, closeBottomSheet, openBottomSheet } = useStore();
+  const { placeId } = useParams(); // URL에서 placeId 추출
   const navigate = useNavigate();
   const location = useLocation();
 
-  // useEffect(() => {
-  //   if (isBottomSheetOpen && selectedPlaceId) {
-  //     navigate(`/places/${selectedPlaceId}`);
-  //   } else if (!isBottomSheetOpen && location.pathname.startsWith('/places')) {
-  //     // 바텀시트 닫혔다면 홈으로 리디렉트. 이거 로직 맞는지 더 제대로 디버깅 필요
-  //     navigate('/');
-  //   }
-  // }, [isBottomSheetOpen, selectedPlaceId, location.pathname, navigate]);
-
+  // URL 변경에 따라 상태 업데이트
   useEffect(() => {
-    // 로직이 괴상하고 뭔가 억지스럽다고 생각함.. 수정 필요
+    if (placeId) {
+      openBottomSheet(placeId);
+    } else {
+      closeBottomSheet();
+    }
+  }, [placeId, openBottomSheet, closeBottomSheet]);
+
+  // 바텀시트 열림 상태 변경에 따라 URL 업데이트
+  useEffect(() => {
     if (isBottomSheetOpen && selectedPlaceId) {
       navigate(`/places/${selectedPlaceId}`);
     } else if (!isBottomSheetOpen && location.pathname === `/places/${selectedPlaceId}`) {
@@ -46,12 +48,7 @@ function App() {
         <Route path="/places/:placeId" element={<Home />} />
         <Route path="places/:placeId/upload-pung" element={<UploadPung />} />
       </Routes>
-      <BottomSheet
-        isOpen={isBottomSheetOpen}
-        placeId={selectedPlaceId}
-        onClose={closeBottomSheet}
-      />
-      {/* showNavBar가 true일 때만 NavBar를 렌더링 */}
+      <Map />
       <Navbar />
     </>
   );

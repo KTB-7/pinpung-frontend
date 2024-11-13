f12a443ab258c9da2646d1632a5159af17844f47
extinctmule <extinctmule@gmail.com>
2024-11-13 12:04:05 +0900

    fix: 마커이미지 에러 해결-blob처리 함수 추가, 이미지 소스 변경

--- src/components/Map/CafeMarker.js
+++ src/components/Map/CafeMarker.js
@@ -3,8 +3,15 @@
 /* global kakao */
 import { useEffect, useRef } from 'react';
 import CafeMarkerIcon from '../../assets/icons/cafe-marker.svg';
+import { cropImage } from '../../utils/imageUtils';
 
 const DEFAULT_MARKER_IMAGE = CafeMarkerIcon; //'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
+
+const fetchImageAsBlob = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) throw new Error('이미지 fetch에 실패했습니다.');
+  return await response.blob();
+};
 
 const CafeMarker = ({ cafes, map, onMarkerClick }) => {
   const markers = useRef({});
@@ -15,18 +22,36 @@ const CafeMarker = ({ cafes, map, onMarkerClick }) => {
 
     const currentMarkers = markers.current;
 
-    cafes.forEach((place) => {
+    cafes.forEach(async (place) => {
       // 중복 마커 방지하기
       if (!currentMarkers[place.placeId]) {
-        const imageUrl = (() => {
-          if (place.hasPung) {
-            return `${process.env.REACT_APP_S3_BASE_URL}/uploaded-images/${place.imageId}`;
-          } else {
-            return DEFAULT_MARKER_IMAGE;
-          }
-        })();
+        // const imageUrl = (() => {
+        //   if (place.hasPung) {
+        //     const imageUrl = `${process.env.REACT_APP_S3_BASE_URL}/uploaded-images/${place.imageId}`;
+        //     const croppedImage = cropImage(imageUrl);
+        //     console.log('imageUrl:', imageUrl, 'croppedImage:', croppedImage);
+        //     return croppedImage;
+        //   } else {
+        //     return DEFAULT_MARKER_IMAGE;
+        //   }
+        // })();
+        const imageUrl = place.hasPung
+          ? `${process.env.REACT_APP_S3_BASE_URL}/uploaded-images/${place.imageId}`
+          : DEFAULT_MARKER_IMAGE;
 
-        const markerImage = new kakao.maps.MarkerImage(imageUrl, new kakao.maps.Size(25, 40));
+        let markerImageUrl = DEFAULT_MARKER_IMAGE; // Default if no crop needed
+
+        if (place.hasPung) {
+          try {
+            const imageBlob = await fetchImageAsBlob(imageUrl);
+            const croppedImage = await cropImage(imageBlob);
+            markerImageUrl = URL.createObjectURL(croppedImage);
+          } catch (error) {
+            console.error('Error cropping image:', error);
+          }
+        }
+
+        const markerImage = new kakao.maps.MarkerImage(markerImageUrl, new kakao.maps.Size(40, 40)); //25, 40
 
         // 마커 생성
         const marker = new kakao.maps.Marker({

b069a441f28043dd4f728f3da67b72013824a94c
extinctmule <extinctmule@gmail.com>
2024-11-15 22:37:45 +0900

    feat: 사용자 프로필 페이지 추가

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,5 +2,27 @@ import { useEffect } from 'react';
 import useStore from '../store/store';
 
 const Profile = () => {
-  return <></>;
+  const { setShowMap } = useStore();
+
+  useEffect(() => {
+    setShowMap(false);
+
+    return () => {
+      setShowMap(true);
+    };
+  }, [setShowMap]);
+
+  const handleLogout = () => {
+    clearAuth();
+    Navigate('/login');
+  };
+
+  return (
+    <>
+      <h1>UserInfo</h1>
+      <button onClick={handleLogout}>로그아웃</button>
+    </>
+  );
 };
+
+export default Profile;

162e634e6674ab3cb5350d797fc51c09d0b4199f
extinctmule <extinctmule@gmail.com>
2024-11-16 21:36:03 +0900

    add: OAuthCallback 컴포넌트로 리다이렉트되게 변경

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Profile from '../pages/Profile';
+import OAuthCallback from '../pages/OAuthCallback';
 import Map from '../components/Map/Map';
 import Navbar from '../components/Navbar';
 import PlaceOverview from '../pages/PlaceOverview';
@@ -16,6 +17,7 @@ const Router = () => {
         <Route path="/profile" element={<Profile />} />
         <Route path="/places/:placeId" element={<PlaceOverview />} />
         <Route path="/places/:placeId/upload-pung" element={<UploadPung />} />
+        <Route path="/login/oauth2/code/kakao" element={<OAuthCallback />} />
       </Routes>
       {showMap && <Map />}
       {showNavbar && <Navbar />}

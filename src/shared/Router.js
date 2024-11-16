a7b3f7cf8f8ed22e90bd1f4e5b0d17277e68ee31
extinctmule <extinctmule@gmail.com>
2024-11-16 18:47:24 +0900

    fix: 로그인 페이지 및 라우팅 제거

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -5,9 +5,7 @@ import Map from '../components/Map/Map';
 import Navbar from '../components/Navbar';
 import PlaceOverview from '../pages/PlaceOverview';
 import UploadPung from '../pages/UploadPung';
-import Login from '../pages/Login';
 import useStore from '../store/store';
-import OAuthCallback from '../pages/OAuthCallback';
 
 const Router = () => {
   const { showMap, showNavbar } = useStore();
@@ -18,8 +16,6 @@ const Router = () => {
         <Route path="/profile" element={<Profile />} />
         <Route path="/places/:placeId" element={<PlaceOverview />} />
         <Route path="/places/:placeId/upload-pung" element={<UploadPung />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/login/oauth2/code/kakao" element={<OAuthCallback />} />
       </Routes>
       {showMap && <Map />}
       {showNavbar && <Navbar />}

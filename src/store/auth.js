39acbc0a774ed9f615f0bdcec2f90e054e0d621b
extinctmule <extinctmule@gmail.com>
2024-11-16 21:26:04 +0900

    fix: 인증 상태관리 수정

--- src/store/auth.js
+++ src/store/auth.js
@@ -2,18 +2,12 @@ import { create } from 'zustand';
 
 const useAuthStore = create((set) => ({
   accessToken: null,
+  userInfo: null,
   isAuthenticated: false,
 
-  setAccessToken: (token) => {
-    set({ accessToken: token });
-    if (token) {
-      localStorage.setItem('accessToken', token);
-    } else {
-      localStorage.removeItem('accessToken');
-    }
-  },
-
-  clearAuth: () => set({ accessToken: null, isAuthenticated: false }),
+  setAccessToken: (token) => set({ accessToken: token, isAuthenticated: !!token }),
+  setUserInfo: (userInfo) => set({ userInfo }),
+  clearAuth: () => set({ accessToken: null, userInfo: null, isAuthenticated: false }),
 }));
 
 export default useAuthStore;

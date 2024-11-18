d4901d699be8a1cf8511d4efe36a11790290c20b
extinctmule <extinctmule@gmail.com>
2024-11-18 20:24:03 +0900

    fix: bottom sheet 닫히게 수정, places/nearby api 수정, 의존성 수정, eslint 경고 무시

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,7 +1,8 @@
 /* 사용자 위치를 기반으로 맵을 로드하고, 맵 이동 시 카페목록 갱신 */
 /* global kakao */
+/* eslint react-hooks/exhaustive-deps: "off" */
 
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getUserLocation } from '../../api/locationApi';
 import { fetchNearbyCafes } from '../../api/placesApi';
@@ -14,8 +15,11 @@ const Map = () => {
   const [userLocation, setUserLocation] = useState({ latitude: 37.400113, longitude: 127.106766 });
   const [cafes, setCafes] = useState([]);
   const [level, setLevel] = useState(3);
+  const [bounds, setBounds] = useState(null);
+  const [isSheetOpen, setIsSheetOpen] = useState(false);
   const navigate = useNavigate();
 
+  // 사용자 위치 가져오기
   useEffect(() => {
     getUserLocation()
       .then((location) => {
@@ -26,88 +30,55 @@ const Map = () => {
       });
   }, []);
 
-  const getRadiusByLevel = (level) => {
-    switch (level) {
-      case 1:
-        return 140;
-      case 2:
-        return 280;
-      default: // level 3 이거나 그 이상
-        return 550;
+  // 맵 변경 이벤트 처리
+  const handleMapChange = () => {
+    if (mapInstance.current) {
+      const newLevel = mapInstance.current.getLevel();
+      setLevel(newLevel);
+
+      const rect = mapInstance.current.getBounds();
+      setBounds(rect);
     }
   };
 
-  useEffect(() => {
-    // `debounce` 처리된 함수 생성
-    const handleMapChangeDebounced = debounce(async () => {
-      if (mapInstance.current) {
-        const newLevel = mapInstance.current.getLevel();
-        setLevel(newLevel);
-        const radius = getRadiusByLevel(newLevel);
-
-        fetchNearbyCafes(userLocation.longitude, userLocation.latitude, radius)
-          .then((data) => setCafes(data.places))
-          .catch((error) => console.error('카페 목록 가져오기 실패:', error));
-      }
-    }, 200);
-
-    // 카카오 맵 이벤트 리스너 추가
-    if (mapInstance.current) {
-      kakao.maps.event.addListener(mapInstance.current, 'dragend', handleMapChangeDebounced);
-      kakao.maps.event.addListener(mapInstance.current, 'zoom_changed', handleMapChangeDebounced);
+  const handleMapClick = () => {
+    if (isSheetOpen) {
+      setIsSheetOpen(false);
+      navigate('/');
     }
+  };
+
+  // 맵 초기화 함수
+  const initializeMap = useCallback(() => {
+    const container = mapRef.current;
+    const options = {
+      center: new kakao.maps.LatLng(userLocation.latitude, userLocation.longitude),
+      level,
+    };
+
+    const map = new kakao.maps.Map(container, options);
+    mapInstance.current = map;
+
+    // 맵 이동 및 줌 변경 시 이벤트 리스너 등록
+    kakao.maps.event.addListener(map, 'dragend', debounce(handleMapChange, 200));
+    kakao.maps.event.addListener(map, 'zoom_changed', debounce(handleMapChange, 200));
+    kakao.maps.event.addListener(map, 'click', handleMapClick);
 
     // 클린업 함수에서 이벤트 리스너 제거
     return () => {
-      if (mapInstance.current) {
-        kakao.maps.event.removeListener(mapInstance.current, 'dragend', handleMapChangeDebounced);
-        kakao.maps.event.removeListener(
-          mapInstance.current,
-          'zoom_changed',
-          handleMapChangeDebounced,
-        );
-      }
-      handleMapChangeDebounced.cancel(); // debounce 취소하여 메모리 누수 방지
+      kakao.maps.event.removeListener(map, 'dragend', handleMapChange);
+      kakao.maps.event.removeListener(map, 'zoom_changed', handleMapChange);
+      kakao.maps.event.removeListener(map, 'click', handleMapClick);
     };
-  }, [userLocation]);
+  }, [userLocation, level]);
 
-  // 초기 맵 로드 및 API 호출
+  // 맵 초기화 및 이벤트 리스너 등록
   useEffect(() => {
     const script = document.createElement('script');
-    // 다음에 이것도 api폴더로 빼보자..
     script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_MAP_KEY}&autoload=false`;
     script.onload = () => {
       kakao.maps.load(() => {
-        const container = mapRef.current;
-        const options = {
-          center: new kakao.maps.LatLng(userLocation.latitude, userLocation.longitude),
-          level: level,
-        };
-        const map = new kakao.maps.Map(container, options);
-        mapInstance.current = map;
-
-        // `debounce` 처리된 함수 생성 및 이벤트 리스너 등록 (이거 위 함수랑 같은거임. 확인후 삭제)
-        const handleMapChangeDebounced = debounce(async () => {
-          if (mapInstance.current) {
-            const newLevel = mapInstance.current.getLevel();
-            setLevel(newLevel);
-            const radius = getRadiusByLevel(newLevel);
-
-            fetchNearbyCafes(userLocation.longitude, userLocation.latitude, radius)
-              .then((data) => setCafes(data.places))
-              .catch((error) => console.error('카페 목록 가져오기 실패:', error));
-          }
-        }, 200);
-
-        kakao.maps.event.addListener(map, 'dragend', handleMapChangeDebounced);
-        kakao.maps.event.addListener(map, 'zoom_changed', handleMapChangeDebounced);
-
-        // 클린업 함수에서 이벤트 리스너 제거
-        return () => {
-          kakao.maps.event.removeListener(map, 'dragend', handleMapChangeDebounced);
-          kakao.maps.event.removeListener(map, 'zoom_changed', handleMapChangeDebounced);
-          handleMapChangeDebounced.cancel(); // debounce 취소하여 메모리 누수 방지
-        };
+        initializeMap();
       });
     };
 
@@ -116,14 +87,39 @@ const Map = () => {
     return () => {
       document.head.removeChild(script);
     };
-  }, [userLocation, level]);
+  }, [initializeMap]);
+
+  // bounds 변경 시 카페 목록 다시 가져오기
+  useEffect(() => {
+    console.log('bounds changed');
+    if (bounds) {
+      const sw = bounds.getSouthWest();
+      const ne = bounds.getNorthEast();
+
+      fetchNearbyCafes(sw.getLng(), sw.getLat(), ne.getLng(), ne.getLat())
+        .then((data) => setCafes(data.places))
+        .catch((error) => console.error('카페 목록 가져오기 실패:', error));
+    }
+  }, [bounds]);
+
+  // userLocation 변경 시 맵 중심 업데이트
+  useEffect(() => {
+    console.log('userLocation changed');
+    if (mapInstance.current) {
+      const map = mapInstance.current;
+      map.setCenter(new kakao.maps.LatLng(userLocation.latitude, userLocation.longitude));
+      const rect = map.getBounds();
+      setBounds(rect);
+    }
+  }, [userLocation]);
 
   const handleMarkerClick = (placeId) => {
     navigate(`/places/${placeId}`);
+    setIsSheetOpen(true);
   };
 
   return (
-    <div ref={mapRef} id="map" style={{ width: '100vw', height: '92vh' }}>
+    <div ref={mapRef} id="map" style={{ width: '100vw', height: '92vh' }} onClick={handleMapClick}>
       {mapInstance.current && (
         <CafeMarker cafes={cafes} map={mapInstance.current} onMarkerClick={handleMarkerClick} />
       )}

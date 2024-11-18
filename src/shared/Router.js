bc87236d6397688f512633ddfb12eb351f4d7187
extinctmule <extinctmule@gmail.com>
2024-11-18 12:26:06 +0900

    add: 라우터에 리뷰 업로드 페이지 경로 추가

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -6,6 +6,7 @@ import Map from '../components/Map/Map';
 import Navbar from '../components/Navbar';
 import PlaceOverview from '../pages/PlaceOverview';
 import UploadPung from '../pages/UploadPung';
+import UploadReview from '../pages/UploadReview';
 import useStore from '../store/store';
 
 const Router = () => {
@@ -17,6 +18,7 @@ const Router = () => {
         <Route path="/profile" element={<Profile />} />
         <Route path="/places/:placeId" element={<PlaceOverview />} />
         <Route path="/places/:placeId/upload-pung" element={<UploadPung />} />
+        <Route path="/places/:placeId/upload-review" element={<UploadReview />} />
         <Route path="/login/oauth2/code/kakao" element={<OAuthCallback />} />
       </Routes>
       {showMap && <Map />}

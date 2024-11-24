615e2b4782d3f6e2a18fda88faec74d9699422a8
extinctmule <extinctmule@gmail.com>
2024-11-25 01:46:55 +0900

    refactor: LocationButton 컴포넌트에서 api처리 및 버튼 핸들링하도록 변경

--- src/components/Map/LocationButton.js
+++ src/components/Map/LocationButton.js
@@ -1,9 +1,22 @@
+import { getUserLocation } from '../../api/locationApi';
 import LocationIcon from '../../assets/icons/compass-icon.svg';
 import styled from 'styled-components';
+
 const LocationButton = () => {
+  const handleLocationClick = async () => {
+    try {
+      const location = await getUserLocation();
+      console.log('현재 위치:', location);
+      // TODO: 받은 위치대로 다시 맵 불러와야함
+    } catch (error) {
+      console.error('위치 요청 실패:', error.message);
+      // TODO: 사용자에게 알리는 UI 추가
+    }
+  };
+
   return (
-    <Wrapper>
-      <img src={LocationIcon} alt="AI 아이콘" />
+    <Wrapper onClick={handleLocationClick}>
+      <img src={LocationIcon} alt="위치 아이콘" />
     </Wrapper>
   );
 };

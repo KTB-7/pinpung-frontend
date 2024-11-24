388b1376667b10fb448b16390d73823056ada6c4
extinctmule <extinctmule@gmail.com>
2024-11-25 00:48:31 +0900

    feat: LocationButton 컴포넌트 추가

---
+++ src/components/Map/LocationButton.js
@@ -0,0 +1,30 @@
+import LocationIcon from '../../assets/icons/compass-icon.svg';
+import styled from 'styled-components';
+const LocationButton = () => {
+  return (
+    <Wrapper>
+      <img src={LocationIcon} alt="AI 아이콘" />
+    </Wrapper>
+  );
+};
+
+export default LocationButton;
+
+const Wrapper = styled.div`
+  position: fixed;
+  bottom: 10%;
+  right: 1%;
+  height: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  margin: 3%;
+  z-index: 2;
+  img {
+    width: 100%;
+    height: 100%;
+    max-width: 60px;
+    max-height: 60px;
+  }
+`;

fa3033d95a24c369a52685bc01846ec781ea8cfd
extinctmule <extinctmule@gmail.com>
2024-11-25 01:01:30 +0900

    design: 정렬방식 변경

--- src/components/Map/AIButton.js
+++ src/components/Map/AIButton.js
@@ -14,18 +14,18 @@ export default AIButton;
 const Wrapper = styled.div`
   position: fixed;
   bottom: 20%;
-  right: 0;
+  right: 0px;
   height: auto;
   display: flex;
   align-items: center;
   justify-content: center;
   cursor: pointer;
-  margin: 3%;
+  margin: 2%;
   z-index: 2;
   img {
     width: 100%;
     height: 100%;
-    max-width: 60px;
-    max-height: 60px;
+    max-width: 65px;
+    max-height: 65px;
   }
 `;

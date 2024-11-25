665009b8b61b36da391604c8f8a856860f8acf84
extinctmule <extinctmule@gmail.com>
2024-11-25 22:44:27 +0900

    update: 파일 선택 부분 UploadReview와 같은 UI로 변경

--- src/pages/UploadPung.js
+++ src/pages/UploadPung.js
@@ -5,6 +5,7 @@ import useAuthStore from '../store/auth';
 import { addPung } from '../api/pungApi';
 import { compressAndPadImage } from '../utils/imageUtils';
 import styled from 'styled-components';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const UploadPung = () => {
   const { placeId } = useParams(); // URL 파라미터에서 placeId를 받아옴
@@ -37,19 +38,55 @@ const UploadPung = () => {
     }
   };
 
+  const handleClose = async () => {
+    navigate(-1);
+  };
+
   return (
     <Wrapper>
-      <PlaceArea>{selectedPlaceName}</PlaceArea>
-      <CenteredArea>
-        <h2>
-          자유롭게 표현해 보세요
-          <br />
-          24시간 뒤에 펑! 돼요
-        </h2>
-        <textarea value={text} onChange={handleTextChange} placeholder="텍스트를 입력하세요" />
-        <input type="file" accept="image/*" onChange={handleImageUpload} />
-      </CenteredArea>
-      <UploadButton onClick={handleUpload}>업로드</UploadButton>
+      <Header className="container-fluid">
+        <PlaceName> {selectedPlaceName} </PlaceName>
+        <CloseButton
+          onClick={handleClose}
+          type="button"
+          className="btn-close"
+          aria-label="Close"
+        ></CloseButton>
+      </Header>
+      <Form className="container-fluid">
+        <CenteredArea className="row w-100">
+          <div className="col-12 text-center mb-4">
+            <h3>
+              자유롭게 표현해 보세요
+              <br /> 24시간 뒤에 펑! 돼요
+            </h3>
+          </div>
+          <div className="col-12 d-flex justify-content-center mb-3">
+            <div className="input-group" style={{ width: 'auto' }}>
+              <input
+                type="file"
+                className="form-control"
+                accept="image/*"
+                onChange={handleImageUpload}
+              />
+              {image && <ImagePreview src={URL.createObjectURL(image)} alt="미리보기" />}
+            </div>
+          </div>
+          <div className="col-12 mb-4">
+            <textarea
+              value={text}
+              onChange={handleTextChange}
+              placeholder="텍스트를 입력하세요"
+              className="form-control"
+              rows="1"
+              style={{ width: '100%' }}
+            />
+          </div>
+        </CenteredArea>
+        <div className="d-flex justify-content-center">
+          <UploadButton onClick={handleUpload}>업로드</UploadButton>
+        </div>
+      </Form>
     </Wrapper>
   );
 };
@@ -59,39 +96,66 @@ export default UploadPung;
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center; /* 수직 중앙 정렬 */
-  width: 100vw; /* 화면의 가로를 모두 채움 */
-  height: 100vh; /* 화면의 세로를 모두 채움 */
-  padding: 20px;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  box-sizing: border-box;
   background-color: #434343;
-  box-sizing: border-box; /* 패딩 포함 전체 크기 조정 */
 `;
 
-const PlaceArea = styled.div`
-  width: 100%;
+const Header = styled.div`
+  position: relative;
+  display: flex;
   text-align: center;
-  color: #ffde59;
-  font-size: 1.5em; /* 적절히 강조하기 위해 폰트 크기 조정 */
-  margin-bottom: 20px;
+  color: white;
+  top: 20px;
+`;
+
+const PlaceName = styled.div`
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%);
+  font-weight: bold;
+  font-size: 1rem;
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  right: 20px;
+  top: 2px;
+  filter: invert(1);
+`;
+
+const Form = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  padding: 20px;
+  box-sizing: border-box;
 `;
 
 const CenteredArea = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center; /* 가로 중앙 정렬 */
-  justify-content: center; /* 세로 중앙 정렬 */
-  text-align: center; /* 텍스트 중앙 정렬 */
-  color: #8c8c8c; /* h2 텍스트 색상 */
-  margin-bottom: 20px;
+  width: 100%;
+  max-width: 500px;
+  color: #8c8c8c;
 `;
 
 const UploadButton = styled.button`
-  margin-top: 10px;
-  padding: 10px 20px;
-  background-color: #ffde59;
-  color: #434343;
+  background-color: #6398f2;
+  color: white;
+  font-weight: bold;
   border: none;
   border-radius: 5px;
+  padding: 10px 30px;
   cursor: pointer;
+`;
+
+const ImagePreview = styled.img`
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+  margin-left: 10px;
 `;

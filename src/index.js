79374d6f5ae937207d40c936fad838108f3742ed
extinctmule <extinctmule@gmail.com>
2024-11-25 19:38:04 +0900

    fix: 인터셉터 셋업 코드 추가

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import { setupRequestInterceptor } from './api/interceptors';
 import GlobalStyle from './styles/GlobalStyles';
 //import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+
+setupRequestInterceptor();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 

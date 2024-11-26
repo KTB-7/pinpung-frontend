f017673d80ae114024a61da6552a23f0de69b5b2
extinctmule <extinctmule@gmail.com>
2024-11-26 22:19:39 +0900

    add: 전역에서 렌더링되던 컴포넌트를 페이지마다 렌더링되게 변경

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import AIButton from '../components/Map/AIButton';
+import LocationButton from '../components/Map/LocationButton';
+import SearchBar from '../components/Map/SearchBar';
 
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <SearchBar />
+      <AIButton />
+      <LocationButton />
+    </>
+  );
 };
 
 export default Home;
--- src/pages/PlaceOverview.js
+++ src/pages/PlaceOverview.js
@@ -1,12 +1,18 @@
 import { useParams } from 'react-router-dom';
+import SearchBar from '../components/Map/SearchBar';
 import BottomSheet from '../components/BottomSheet';
+import AIButton from '../components/Map/AIButton';
+import LocationButton from '../components/Map/LocationButton';
 
 const PlaceOverview = () => {
   const { placeId } = useParams();
 
   return (
     <>
+      <SearchBar />
       <BottomSheet placeId={placeId} />
+      <AIButton />
+      <LocationButton />
     </>
   );
 };

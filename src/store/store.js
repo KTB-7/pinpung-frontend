a4b7b273cd409d4ec1db57ef2d920214d0cecf8e
extinctmule <extinctmule@gmail.com>
2024-11-28 13:35:36 +0900

    add:center과 rect 상태관리에 추가

--- src/store/store.js
+++ src/store/store.js
@@ -8,12 +8,16 @@ const useStore = create((set) => ({
   selectedPlaceId: null,
   selectedPlaceName: '',
   selectedNavbar: 'home',
+  center: null,
+  mapRect: null,
 
   setShowMap: (show) => set({ showMap: show }),
   setShowNavbar: (show) => set({ showNavbar: show }),
   setSelectedPlaceId: (placeId) => set({ selectedPlaceId: placeId }),
   setSelectedPlaceName: (placeName) => set({ selectedPlaceName: placeName }),
   setSelectedNavbar: (icon) => set({ selectedNavbar: icon }),
+  setCenter: (location) => set({ center: location }),
+  setMapRect: (rect) => set({ mapRect: rect }),
 }));
 
 export default useStore;

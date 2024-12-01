338f451ae36e493d7c86e4e10151ac28c9b81442
extinctmule <extinctmule@gmail.com>
2024-12-01 23:22:32 +0900

    refactor: useState대신 useStore로 location 처리하게 변경

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -14,27 +14,29 @@ import { debounce } from 'lodash';
 const Map = () => {
   const mapRef = useRef(null);
   const mapInstance = useRef(null);
-  const userInfo = useAuthStore((state) => state.userInfo);
-  const setCenter = useStore((state) => state.setCenter);
+  const navigate = useNavigate();
 
-  const [userLocation, setUserLocation] = useState({ latitude: 37.400113, longitude: 127.106766 });
+  const center = useStore((state) => state.center);
+  const setCenter = useStore((state) => state.setCenter);
+  const userInfo = useAuthStore((state) => state.userInfo);
+
   const [cafes, setCafes] = useState([]);
   const [level, setLevel] = useState(3);
   const [bounds, setBounds] = useState(null);
   const [isSheetOpen, setIsSheetOpen] = useState(false);
-  const navigate = useNavigate();
 
   // 사용자 위치 가져오기
   useEffect(() => {
-    getUserLocation()
-      .then((location) => {
-        setUserLocation(location);
-      })
-      .catch((error) => {
-        console.error('위치 정보를 가져오는 중 오류 발생', error);
-      });
-    setCenter({ latitude: userLocation.latitude, longitude: userLocation.longitude });
-  }, []);
+    if (!center) {
+      getUserLocation()
+        .then((location) => {
+          setCenter({ latitude: location.latitude, longitude: location.longitude });
+        })
+        .catch((error) => {
+          console.error('위치 정보를 가져오는 중 오류 발생', error);
+        });
+    }
+  }, [center, setCenter]);
 
   // 맵 변경 이벤트 처리
   const handleMapChange = () => {
@@ -58,7 +60,7 @@ const Map = () => {
   const initializeMap = useCallback(() => {
     const container = mapRef.current;
     const options = {
-      center: new kakao.maps.LatLng(userLocation.latitude, userLocation.longitude),
+      center: new kakao.maps.LatLng(center.latitude, center.longitude),
       level,
     };
 
@@ -76,24 +78,26 @@ const Map = () => {
       kakao.maps.event.removeListener(map, 'zoom_changed', handleMapChange);
       kakao.maps.event.removeListener(map, 'click', handleMapClick);
     };
-  }, [userLocation, level]);
+  }, [center, level]);
 
   // 맵 초기화 및 이벤트 리스너 등록
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_MAP_KEY}&autoload=false`;
-    script.onload = () => {
-      kakao.maps.load(() => {
-        initializeMap();
-      });
-    };
+    if (center) {
+      const script = document.createElement('script');
+      script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_MAP_KEY}&autoload=false`;
+      script.onload = () => {
+        kakao.maps.load(() => {
+          initializeMap();
+        });
+      };
 
-    document.head.appendChild(script);
+      document.head.appendChild(script);
 
-    return () => {
-      document.head.removeChild(script);
-    };
-  }, [initializeMap]);
+      return () => {
+        document.head.removeChild(script);
+      };
+    }
+  }, [center, initializeMap]);
 
   // bounds 변경 시 카페 목록 다시 가져오기
   useEffect(() => {
@@ -107,17 +111,6 @@ const Map = () => {
         .catch((error) => console.error('카페 목록 가져오기 실패:', error));
     }
   }, [bounds]);
-
-  // userLocation 변경 시 맵 중심 업데이트
-  useEffect(() => {
-    console.log('userLocation changed');
-    if (mapInstance.current) {
-      const map = mapInstance.current;
-      map.setCenter(new kakao.maps.LatLng(userLocation.latitude, userLocation.longitude));
-      const rect = map.getBounds();
-      setBounds(rect);
-    }
-  }, [userLocation]);
 
   const handleMarkerClick = (placeId) => {
     navigate(`/places/${placeId}`);

0c2c3292367891c8dbf9d837a50e7c1850ac967c
extinctmule <extinctmule@gmail.com>
2024-12-04 21:47:06 +0900

    feat: 페이지 구성하고, props로 SearchResultList 렌더링하게 처리

--- src/pages/DefaultSearch.js
+++ src/pages/DefaultSearch.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import { useSearchParams } from 'react-router-dom';
+import useAuthStore from '../store/auth';
+import useStore from '../store/store';
+import SearchResultList from './SearchResultList';
 
 const DefaultSearch = () => {
-  return <></>;
+  const userId = useAuthStore((state) => state.userInfo?.userId);
+  const bounds = useStore((state) => state.bounds);
+  const [searchParams] = useSearchParams();
+  const keyword = searchParams.get('keyword');
+  const sort = searchParams.get('sort') || 'accuracy';
+
+  return (
+    <div>
+      <SearchBar />
+      {keyword && (
+        <SearchResultList keyword={keyword} sort={sort} bounds={bounds} userId={userId} />
+      )}
+      <AIButton />
+      <LocationButton />
+    </div>
+  );
 };
 
 export default DefaultSearch;

f40d30f1739fd749d3c0823659035514480f2385
extinctmule <extinctmule@gmail.com>
2024-12-05 11:36:06 +0900

    design: searchbar 패딩 처리

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,20 @@ import React from 'react';
 import AIButton from '../components/Map/AIButton';
 import LocationButton from '../components/Map/LocationButton';
 import SearchBar from '../components/Map/SearchBar';
+import { Container } from 'react-bootstrap';
 
 const Home = () => {
   return (
     <>
-      <SearchBar />
+      <Container
+        fluid
+        className="d-flex flex-column"
+        style={{
+          paddingTop: '1rem',
+        }}
+      >
+        <SearchBar />
+      </Container>
       <AIButton />
       <LocationButton />
     </>

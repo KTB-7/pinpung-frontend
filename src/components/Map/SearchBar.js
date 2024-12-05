0d8dfe1fd94f3d5114c7cc9e3c2df66589db9747
extinctmule <extinctmule@gmail.com>
2024-12-05 11:36:46 +0900

    feat: 검색어가 창에 유지되도록 변경

--- src/components/Map/SearchBar.js
+++ src/components/Map/SearchBar.js
@@ -1,17 +1,21 @@
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { Form } from 'react-bootstrap';
 
 const SearchBar = () => {
-  //const setKeyword = useSearch((state) => state.setKeyword);
   const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+
+  const [keyword, setKeyword] = useState(searchParams.get('keyword') || '');
+
+  const handleInputChange = (event) => {
+    setKeyword(event.target.value);
+  };
 
   // 검색 입력 핸들링
   const handleSearch = (event) => {
     if (event.key === 'Enter') {
       event.preventDefault();
-      const keyword = event.target.value;
-      // setKeyword(keyword);
-
       navigate(`/search-results?keyword=${keyword}&sort=accuracy`);
     }
   };
@@ -25,17 +29,21 @@ const SearchBar = () => {
   };
 
   return (
-    <Form className="d-flex" onClick={handleSearchBarClick}>
-      <Form.Control
-        type="search"
-        placeholder="검색..."
-        className="me-2"
-        aria-label="Search"
-        id="home-search-input"
-        style={{ height: '45px', flex: 1 }}
-        onKeyDown={handleSearch}
-      />
-    </Form>
+    <div style={{ zIndex: 2 }}>
+      <Form className="d-flex" onClick={handleSearchBarClick}>
+        <Form.Control
+          type="search"
+          placeholder="검색..."
+          className="me-2"
+          aria-label="Search"
+          id="home-search-input"
+          style={{ height: '45px', flex: 1 }}
+          value={keyword}
+          onChange={handleInputChange}
+          onKeyDown={handleSearch}
+        />
+      </Form>
+    </div>
   );
 };
 

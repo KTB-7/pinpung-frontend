6f692328e5a8b2e78d3768299a63b52ae0ff921f
extinctmule <extinctmule@gmail.com>
2024-12-05 10:56:37 +0900

    design: Container로 감싸서 UI 통일

--- src/pages/DefaultSearch.js
+++ src/pages/DefaultSearch.js
@@ -1,12 +1,21 @@
 import SearchBar from '../components/Map/SearchBar';
 import SearchResultList from '../components/SearchResultList';
+import { Container } from 'react-bootstrap';
 
 const DefaultSearch = () => {
   return (
-    <div>
+    <Container
+      fluid
+      className="d-flex flex-column"
+      style={{
+        height: '100vh',
+        overflowY: 'auto',
+        paddingTop: '1rem',
+      }}
+    >
       <SearchBar />
       <SearchResultList />
-    </div>
+    </Container>
   );
 };
 

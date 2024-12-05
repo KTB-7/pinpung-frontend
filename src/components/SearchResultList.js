89dbb85318b14630a8a68aab90ed304038181a66
extinctmule <extinctmule@gmail.com>
2024-12-05 10:53:34 +0900

    design: 버튼과 리스트의 마진 및 크기 변경

--- src/components/SearchResultList.js
+++ src/components/SearchResultList.js
@@ -70,26 +70,29 @@ const SearchResultList = () => {
       <div
         className="d-flex justify-content-left mb-3"
         style={{
-          marginTop: '1.5rem',
+          marginTop: '0.5rem',
         }}
       >
         <Button
           variant={sort === 'accuracy' ? 'secondary' : 'outline-secondary'}
           onClick={() => handleSortChange('accuracy')}
-          style={{ marginRight: '1rem' }}
+          style={{ marginRight: '0.5rem', height: '30px' }}
+          size="sm" // 버튼 크기 줄이기
         >
           정확도순
         </Button>
         <Button
           variant={sort === 'distance' ? 'secondary' : 'outline-secondary'}
           onClick={() => handleSortChange('distance')}
+          style={{ height: '30px' }}
+          size="sm"
         >
           거리순
         </Button>
       </div>
 
       {/* 검색 결과 리스트 */}
-      <ListGroup>
+      <ListGroup style={{ overflowY: 'auto', height: 'calc(100vh - 120px)' }}>
         {searchResults.map((place) => (
           <ListGroup.Item
             key={place.placeId}

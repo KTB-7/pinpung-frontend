7c257557f019dfc9bd1ebb9dbb3acdb58944b52c
extinctmule <extinctmule@gmail.com>
2024-12-20 05:33:55 +0900

    add: AIHome 페이지 구현

--- src/pages/AIHome.js
+++ src/pages/AIHome.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import AIRecommendList from '../components/AIRecommendList';
 import HomeButton from '../components/Map/HomeButton';
 import LocationButton from '../components/Map/LocationButton';
-import SearchBar from '../components/Map/SearchBar';
-import { Container } from 'react-bootstrap';
+import styled from 'styled-components';
 
 const AIHome = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const view = searchParams.get('view') || 'map';
+
+  const handleToggleView = () => {
+    setSearchParams({ view: view === 'map' ? 'list' : 'map' });
+  };
+
   return (
     <>
       <Container
@@ -14,7 +23,9 @@ const AIHome = () => {
           paddingTop: '1rem',
         }}
       >
-        <SearchBar />
+        <ToggleButton onClick={handleToggleView}>{view === 'map' ? '리스트' : '맵'}</ToggleButton>
+        {view === 'list' && <AIRecommendList />}
+        {/* TODO: ㅇㅇ님을 위한 카페 추천 문구 넣기 */}
       </Container>
       <HomeButton />
       <LocationButton />
@@ -23,3 +34,10 @@ const AIHome = () => {
 };
 
 export default AIHome;
+
+const ToggleButton = styled.button`
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  z-index: 10;
+`;

8f484ec3bb4cfc4dc629a35161abc92e9afbef1f
extinctmule <extinctmule@gmail.com>
2024-12-24 00:28:30 +0900

    add: mockdata 변경

--- src/mocks/data/AIRecommendData.js
+++ src/mocks/data/AIRecommendData.js
@@ -1,6 +1,7 @@
 // src/mocks/data/AIRecommendData.js
 
 export const AIRecommendData = {
+  // request parameters
   // swLng: 127.10455447603056
   // swLat: 37.39668744674147
   // neLng: 127.10896881707035
@@ -9,13 +10,13 @@ export const AIRecommendData = {
   // x: 37.400113
   // y: 127.106766
 
-  count: 3,
+  count: 6,
   places: [
     {
       placeId: 26,
       placeName: '빙스퐁 판교2호점',
       address: '경기 성남시 분당구 대왕판교로 670',
-      tags: ['가짜태그'],
+      tags: ['가짜태그1'],
       imageId: 0,
       x: '127.107078257823',
       y: '37.4020354514922',
@@ -37,6 +38,32 @@ export const AIRecommendData = {
       imageId: 0,
       x: '127.107430636977',
       y: '37.4020351318533',
+    },
+    {
+      address: '경기 성남시 분당구 판교역로 235',
+      placeId: 36,
+      placeName: '투썸플레이스 판교테크노밸리점',
+      tags: ['가짜태그4'],
+      imageId: 0,
+      x: '127.10851032513395',
+      y: '37.402008016343174',
+    },
+    {
+      address: '경기 성남시 분당구 대왕판교로645번길 12',
+      placeId: 21,
+      placeName: '블루포트 경기창조경제혁신센터점',
+      tags: ['가짜태그5'],
+      imageId: 0,
+      x: '127.104876885413',
+      y: '37.4003218808989',
+    },
+    {
+      address: '경기 성남시 분당구 대왕판교로 660',
+      placeId: 30,
+      placeName: '해밀 판교점',
+      tags: ['가짜태그6'],
+      x: '127.10714197082693',
+      y: '37.40077576672964',
     },
   ],
 };

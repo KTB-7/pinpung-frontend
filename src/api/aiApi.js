a5b510fac29bdd5d823a7aeee3033489863d76cf
extinctmule <extinctmule@gmail.com>
2024-12-24 00:16:46 +0900

    feat: /api/ai/recommend mockdata 세팅

--- src/api/aiApi.js
+++ src/api/aiApi.js
@@ -1,9 +1,27 @@
 import { securedInstance } from './axiosInstance';
+import { AIRecommendData } from '../mocks/data/AIRecommendData';
 
+const USE_MOCK_DATA = process.env.REACT_APP_USE_MOCK_DATA === 'true';
 const API_URL = process.env.REACT_APP_API_URL;
 
-// AI 사용자 맞춤 태그와 함께 근처 카페 추천하기
+/**
+ * AI 사용자 맞춤 태그와 함께 근처 카페 추천하기
+ * @param {number} swLng - 남서쪽 경도
+ * @param {number} swLat - 남서쪽 위도
+ * @param {number} neLng - 북동쪽 경도
+ * @param {number} neLat - 북동쪽 위도
+ * @param {number} x - 사용자 경도
+ * @param {number} y - 사용자 위도
+ * @returns {Promise<Object>} - 카페 추천 데이터
+ */
 export const fetchAIRecommendCafes = async (swLng, swLat, neLng, neLat, x, y) => {
+  if (USE_MOCK_DATA) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(AIRecommendData);
+      }, 500);
+    });
+  }
   try {
     const response = await securedInstance.get(`${API_URL}/api/ai/recommend`, {
       params: {
---
+++ src/mocks/data/AIRecommendData.js
@@ -0,0 +1,42 @@
+// src/mocks/data/AIRecommendData.js
+
+export const AIRecommendData = {
+  // swLng: 127.10455447603056
+  // swLat: 37.39668744674147
+  // neLng: 127.10896881707035
+  // neLat: 37.40353122045034
+
+  // x: 37.400113
+  // y: 127.106766
+
+  count: 3,
+  places: [
+    {
+      placeId: 26,
+      placeName: '빙스퐁 판교2호점',
+      address: '경기 성남시 분당구 대왕판교로 670',
+      tags: ['가짜태그'],
+      imageId: 0,
+      x: '127.107078257823',
+      y: '37.4020354514922',
+    },
+    {
+      address: '경기 성남시 분당구 대왕판교로 670',
+      placeId: 29,
+      placeName: '바이브커피',
+      tags: ['가짜태그2'],
+      imageId: 0,
+      x: '127.106111529864',
+      y: '37.4020759679173',
+    },
+    {
+      address: '경기 성남시 분당구 대왕판교로 670',
+      placeId: 14,
+      placeName: '매머드커피 판교점',
+      tags: ['가짜태그3'],
+      imageId: 0,
+      x: '127.107430636977',
+      y: '37.4020351318533',
+    },
+  ],
+};

943ab0d0ecd14e8f74e11290d1c2cb745e5a8b44
extinctmule <extinctmule@gmail.com>
2024-12-25 13:27:45 +0900

    fix: 파라미터 명시적 체크하도록 수정

--- src/hooks/useAIRecommendCafes.js
+++ src/hooks/useAIRecommendCafes.js
@@ -7,7 +7,15 @@ const useAIRecommendCafes = (swLng, swLat, neLng, neLat, x, y) => {
     queryFn: () => fetchAIRecommendCafes(swLng, swLat, neLng, neLat, x, y),
     staleTime: 5 * 60 * 1000,
     cacheTime: 30 * 60 * 1000,
-    enabled: !!swLng && !!swLat && !!neLng && !!neLat && !!x && !!y, // 필요한 파라미터가 모두 있을 때만 실행하자
+
+    // 명시적 체크 해야함
+    enabled:
+      swLng !== null &&
+      swLat !== null &&
+      neLng !== null &&
+      neLat !== null &&
+      x !== null &&
+      y !== null,
   });
 };
 

05760925840ad454ed6f4a5d1996431160fe5cd2
extinctmule <extinctmule@gmail.com>
2024-12-25 13:26:16 +0900

    add: 펑 목록 조회 api 추가

--- src/api/pungApi.js
+++ src/api/pungApi.js
@@ -2,17 +2,25 @@ import { securedInstance } from './axiosInstance';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-// export const fetchPungsByPlace = async (placeId, page) => {
-//   try {
-//     const response = await securedInstance.get(`${API_URL}/api/pungs/byPlace/${placeId}`, page);
+// 특정 장소의 펑 목록 조회
+export const fetchPungsByPlace = async (placeId, page, size) => {
+  try {
+    const response = await securedInstance.get(`${API_URL}/api/pungs/byPlace/${placeId}`, {
+      params: {
+        placeId,
+        page,
+        size,
+      },
+    });
 
-//     return response.data;
-//   } catch (error) {
-//     console.error('펑 가져오기 실패:', error);
-//     throw new Error('펑을 가져오는 데 실패했습니다.');
-//   }
-// };
+    return response.data;
+  } catch (error) {
+    console.error('펑 가져오기 실패:', error);
+    throw new Error('펑을 가져오는 데 실패했습니다.');
+  }
+};
 
+// 펑 업로드
 export const addPung = async (placeId, imageWithText, pureImage, text) => {
   const data = new FormData();
 
